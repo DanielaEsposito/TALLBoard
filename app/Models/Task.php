50fca0a035adab56e8e2112f4c1256ad12e70153
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-06-09 10:18:03 +0200

    add register and login form to welcome page

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,4 +10,8 @@ class Task extends Model
     {
         return $this->belongsTo(Project::class);
     }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,15 +29,115 @@
         <!-- Colonna Login -->
         <div class="w-1/2 flex items-center flex-col justify-center bg-white z-0">
             <h1 class="text-2xl font-bold text-gray-900 mb-4">Login</h1>
-            <!-- Qui potresti mettere il form di login -->
-            <button x-on:click="register = false" class="text-blue-500 underline mt-4">Vai a Registrazione</button>
+
+            <!-- Session Status -->
+            <x-auth-session-status class="mb-4" :status="session('status')" />
+
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+
+                <!-- Email Address -->
+                <div>
+                    <x-input-label for="email" :value="__('Email')" />
+                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email"
+                        :value="old('email')" required autofocus autocomplete="username" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+
+                <!-- Password -->
+                <div class="mt-4">
+                    <x-input-label for="password" :value="__('Password')" />
+
+                    <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                        autocomplete="current-password" />
+
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
+
+                <!-- Remember Me -->
+                <div class="block text-center mt-4">
+                    <label for="remember_me" class="inline-flex items-center">
+                        <input id="remember_me" type="checkbox"
+                            class="rounded border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+                            name="remember">
+                        <span class="ms-2 text-sm  text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                    </label>
+                </div>
+
+                <div class="flex flex-col items-center mt-4 mb-4 space-y-2">
+                    <x-primary-button class="ms-3">
+                        {{ __('Log in') }}
+                    </x-primary-button>
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                            href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    @endif
+                </div>
+
+                <div class="flex flex-col items-center mt-4 mb-4 space-y-2">
+
+                    <span class=" ms-2 text-sm text-gray-600 dark:text-gray-400">Non hai un account ?</span>
+                    <button x-on:click="register = false" class="text-blue-500 underline mt-2">Registrati</button>
+                </div>
+            </form>
         </div>
 
         <!-- Colonna Registrazione -->
-        <div class="w-1/2 flex items-center flex-col justify-center bg-gray-100 z-0">
+        <div class="w-1/2 flex items-center flex-col justify-center z-0">
             <h1 class="text-2xl font-bold text-gray-900 mb-4">Registrati</h1>
-            <!-- Qui potresti mettere il form di registrazione -->
-            <button x-on:click="register = true" class="text-blue-500 underline mt-4">Torna al Login</button>
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+
+                <!-- Name -->
+                <div>
+                    <x-input-label for="name" :value="__('Name')" />
+                    <x-text-input id="name" class="block mt-1 w-full" type="text" name="name"
+                        :value="old('name')" required autofocus autocomplete="name" />
+                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                </div>
+
+                <!-- Email Address -->
+                <div class="mt-4">
+                    <x-input-label for="email" :value="__('Email')" />
+                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email"
+                        :value="old('email')" required autocomplete="username" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+
+                <!-- Password -->
+                <div class="mt-4">
+                    <x-input-label for="password" :value="__('Password')" />
+
+                    <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                        autocomplete="new-password" />
+
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
+
+                <!-- Confirm Password -->
+                <div class="mt-4">
+                    <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                    <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                        name="password_confirmation" required autocomplete="new-password" />
+
+                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                </div>
+
+                <div class="text-center mt-4">
+
+
+                    <x-primary-button class="ms-4">
+                        {{ __('Register') }}
+                    </x-primary-button>
+                </div>
+                <div class="flex flex-col items-center mt-4 mb-4 space-y-2">
+                    <span class=" ms-2 text-sm text-gray-600 dark:text-gray-400">Hai già un account ?</span>
+                    <button x-on:click="register = true" class="text-blue-500 underline mt-4">Torna al Login</button>
+                </div>
+            </form>
         </div>
     </div>
 
