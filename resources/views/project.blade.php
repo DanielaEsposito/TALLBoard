b125867dcc920aa6c9035be5c4ddc68d1018cfc9
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-06-10 10:34:31 +0200

    add sidebar

---
+++ resources/views/project.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container mx-auto">
+        <h1>Progetti</h1>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/projects', function () {
+    return view('projects');
+})->middleware(['auth', 'verified'])->name('projects');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,4 +20,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
